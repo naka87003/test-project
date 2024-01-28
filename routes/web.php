e75b983a007beac565d0e60100ed3901570de184
naka87003 <naka87003@outlook.jp>
2024-01-28 17:30:29 +0900

    middlewareのadminを作成し、ルートに適用

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('post', PostController::class)->except(['update', 'destroy']);
+    Route::resource('post', PostController::class)->except(['update', 'destroy'])->middleware('admin');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
