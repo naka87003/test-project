b4323e9c8c0281837590ac6f61fcdde274b7847c
naka87003 <naka87003@outlook.jp>
2024-01-28 17:31:09 +0900

    Gateのtestを作成し、welcome.bladeに適用

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
@@ -38,6 +39,8 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        Gate::authorize('test');
+        
         $validated = $request->validate([
             'title' => 'required|max:20',
             'body' => 'required|max:400'
