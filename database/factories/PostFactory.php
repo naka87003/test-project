bf0131130138b2a0941c573002775b261bdbade6
naka87003 <naka87003@outlook.jp>
2024-02-03 14:41:38 +0900

    Factoryを作成

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(20),
+            'body' => fake()->text(50),
+            'user_id' => User::factory()
+        ];
+    }
+}
