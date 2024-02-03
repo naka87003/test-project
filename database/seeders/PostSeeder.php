e1c1b2543fb31165981e7e49976ddbad6551906a
naka87003 <naka87003@outlook.jp>
2024-02-03 10:15:18 +0900

    PostSeederを作成

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::create([
+            'title' => 'テスト',
+            'body' => 'シーダーのテストを実施します。',
+            'user_id' => 1,
+        ]);
+    }
+}
